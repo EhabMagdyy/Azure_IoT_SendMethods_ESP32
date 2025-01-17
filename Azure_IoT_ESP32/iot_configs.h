427944401b6740bc0d711629783025d3e355eb65
EhabMadgyy <ehabmagdy0044@gmail.com>
2025-01-17 16:26:40 +0200

    ESP32 Code

---
+++ Azure_IoT_ESP32/iot_configs.h
@@ -0,0 +1,14 @@
+/********************************************************************************************************************
+*****************************    Author  : Ehab Magdy Abdullah                      *********************************
+*****************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *********************************
+*****************************    Youtube : https://www.youtube.com/@EhabMagdyy      *********************************
+*********************************************************************************************************************/
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "PASSWORD"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "<IoTHubName>.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "DEVICE_ID"
+#define IOT_CONFIG_DEVICE_KEY "DEVICE_KEY"
